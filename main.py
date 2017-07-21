be893a75d5438bda9c6c905a7d948c0494631f1e
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-07-21 18:52:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from sys import argv
+from time import sleep
+from plexapi.server import PlexServer
+
+print('arvg', argv)
+url = argv[1]
+token = argv[2]
+section_title = argv[3]
+playlist_title = argv[4]
+item_title = argv[5]
+
+def cb(data):
+    if 'StatusNotification' and data['size'] == 1:
+        status = data[u'StatusNotification'][0]
+        if 'title' in status and status['title'] == 'Library scan complete':
+            print('finished refresing')
+            notifier.stop()
+
+
+server = PlexServer(url, token)
+library = server.library
+section = library.section(section_title)
+notifier = server.startAlertListener(callback=cb)
+
+if section.refreshing:
+    print('already refrehsing')
+else:
+    print('start refrehsing')
+    section.update()
+
+while notifier.isAlive():
+    sleep(0.1)
+
+print('addig item to playlist')
+items = library.search(title=item_title, sort='addedAt:desc', maxresults=1)
+print('items', items)
+if items:
+  playlist = server.playlist(playlist_title)
+  print('playlist', playlist)
+  playlist.addItems(items)
+  print('item added to playlist')
+else:
+  print('no items found')
